a367cbac5f8766c235642fd7e958ed412e5466a9
Felix Naumann <felix.naumann@student.uni-luebeck.de>
2022-09-07 18:32:51 +0200

    don't convert pk column to upper case enum

--- src/main/java/org/example/EnumStammdatenGenerator.java
+++ src/main/java/org/example/EnumStammdatenGenerator.java
@@ -4,6 +4,7 @@ import org.jooq.codegen.JavaGenerator;
 import org.jooq.codegen.JavaWriter;
 import org.jooq.meta.Database;
 import org.jooq.meta.SchemaDefinition;
+import org.jooq.meta.TableDefinition;
 import org.jooq.tools.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -43,6 +44,7 @@ public class EnumStammdatenGenerator extends JavaGenerator {
      */
     @Override
     protected void generateSchema(SchemaDefinition schema) {
+        log.info(schema.getDatabase().getTables().toString());
         log.info("Generating enums");
         log.info("--------------------------------------");
         Database db = schema.getDatabase();
@@ -60,104 +62,110 @@ public class EnumStammdatenGenerator extends JavaGenerator {
                         log.warn("Skipping enum generation for the table '{}'", tableDefinition.getName());
                         return;
                     }
+                    generateEnum(schema, tableDefinition, db);
 
-                    String enumName = StringUtils.toCamelCase(tableDefinition.getName()) + "Enum";
-                    File file = new File(getFile(schema).getParentFile().getAbsolutePath(), enumName + ".java");
-                    file.getParentFile().mkdirs();
-                    try {
-                        file.createNewFile();
-                    } catch (IOException e) {
-                        throw new RuntimeException("Failed to create package files for enum generation.", e);
-                    }
-                    JavaWriter out = new JavaWriter(file, enumName + ".java");
-                    log.info("Generating enum: {}.java [input={}, output={}]", enumName, tableDefinition.getName(), enumName);
-                    printPackage(out, schema);
-
-                    out.println(String.format("public enum %s {", enumName));
-
-                    try {
-                        ResultSet resultSet = db.getConnection().prepareStatement(String.format("SELECT * FROM %s.%s", schema.getName(), tableDefinition.getName())).executeQuery();
-                        Map<String, String> variableTypes = new LinkedHashMap<>();
-                        while(resultSet.next()) {
-                            int colCount = resultSet.getMetaData().getColumnCount();
-                            for(int col=1; col<=colCount; col++) {
-                                String column = resultSet.getString(col);
-                                // this is the primary key for this master data table
-                                if(tableDefinition.getColumn(resultSet.getMetaData().getColumnLabel(col)).getPrimaryKey() != null) {
-                                    String name = resultSet.getString(col).toUpperCase();
-                                    name = toValidJavaEnumName(name);
-                                    out.print(String.format("%s(", name));
-                                    continue;
-                                }
-                                //this is any additional column belonging to the enums constructor parameter
-                                String delimiter = col == colCount ? "" : ", ";
-                                String columnClassName = Class.forName(resultSet.getMetaData().getColumnClassName(col)).getSimpleName();
-                                log.info(columnClassName);
-                                String columnLabel = resultSet.getMetaData().getColumnLabel(col);
-                                if(columnClassName.equals("Clob") || columnClassName.equals("String")) {
-                                    variableTypes.put(StringUtils.toLC(columnLabel), "String");
-                                    out.print(String.format("\"%s\"", column));
-                                }
-                                else if(columnClassName.equals("Byte")) {
-                                    variableTypes.put(StringUtils.toLC(columnLabel), "boolean");
-                                    boolean toWrite = Byte.parseByte(column) != 0;
-                                    out.print(Boolean.toString(toWrite));
-                                }
-                                else {
-                                    variableTypes.put(StringUtils.toLC(columnLabel), columnClassName);
-                                    out.print(column);
-                                }
-                                out.print(delimiter);
-                            }
-                            String s = resultSet.isLast() ? ";" : ",";
-                            out.println(String.format(")%s", s));
-                        }
-
-                        out.println();
-
-                        //class variables
-                        variableTypes.forEach((varName, varType) -> {
-                            out.println(String.format("private final %s %s;", varType, varName));
-                        });
-
-                        out.println();
-
-                        //enum constructor definition
-                        String constructorParameters = variableTypes.entrySet().stream()
-                                .map(varNamevarTypeEntry -> String.format("%s %s", varNamevarTypeEntry.getValue(), varNamevarTypeEntry.getKey()))
-                                .collect(Collectors.joining(","));
-                        out.println(String.format("private %s(%s) {", enumName, constructorParameters));
-                        variableTypes.forEach((varName, varType) -> {
-                            out.println(String.format("this.%s = %s;", varName, varName));
-                        });
-                        out.println("}");
-
-                        out.println();
-
-                        //getter methods for class variables
-                        variableTypes.forEach((varName, varType) -> {
-                            if(varType.equals("boolean")) {
-                                out.println(String.format("public %s is%s() {", varType, StringUtils.toUC(varName)));
-                            }
-                            else {
-                                out.println(String.format("public %s get%s() {", varType, StringUtils.toUC(varName)));
-                            }
-                            out.println(String.format("return this.%s;", varName));
-                            out.println("}");
-                            out.println();
-                        });
-
-                        out.println("}");
-
-                        closeJavaWriter(out);
-                    } catch (SQLException | ClassNotFoundException e) {
-                        e.printStackTrace();
-                    }
         });
 
         log.info("--------------------------------------");
 
         super.generateSchema(schema);
+    }
+
+    private void generateEnum(SchemaDefinition schema, TableDefinition tableDefinition, Database db) {
+        String enumName = StringUtils.toCamelCase(tableDefinition.getName()) + "Enum";
+        File file = new File(getFile(schema).getParentFile().getAbsolutePath(), enumName + ".java");
+        file.getParentFile().mkdirs();
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to create package files for enum generation.", e);
+        }
+        JavaWriter out = new JavaWriter(file, enumName + ".java");
+        log.info("Generating enum: {}.java [input={}, output={}]", enumName, tableDefinition.getName(), enumName);
+        printPackage(out, schema);
+
+        out.println(String.format("public enum %s {", enumName));
+
+        try {
+            ResultSet resultSet = db.getConnection().prepareStatement(String.format("SELECT * FROM %s.%s", schema.getName(), tableDefinition.getName())).executeQuery();
+            Map<String, String> variableTypes = new LinkedHashMap<>();
+            while(resultSet.next()) {
+                int colCount = resultSet.getMetaData().getColumnCount();
+                for(int col=1; col<=colCount; col++) {
+                    String column = resultSet.getString(col);
+                    // this is the primary key for this master data table
+                    if(tableDefinition.getColumn(resultSet.getMetaData().getColumnLabel(col)).getPrimaryKey() != null) {
+                        String name = resultSet.getString(col);//.toUpperCase();
+                        name = toValidJavaEnumName(name); //TODO: if this method changes anything a custom converter will be required at runtime for mapping.
+                        out.print(String.format("%s(", name));
+                        continue;
+                    }
+                    //this is any additional column belonging to the enums constructor parameter
+                    String delimiter = col == colCount ? "" : ", ";
+                    String columnClassName = Class.forName(resultSet.getMetaData().getColumnClassName(col)).getSimpleName();
+                    log.info(columnClassName);
+                    String columnLabel = resultSet.getMetaData().getColumnLabel(col);
+                    if(columnClassName.equals("Clob") || columnClassName.equals("String")) {
+                        variableTypes.put(StringUtils.toLC(columnLabel), "String");
+                        out.print(String.format("\"%s\"", column));
+                    }
+                    else if(columnClassName.equals("Byte")) {
+                        variableTypes.put(StringUtils.toLC(columnLabel), "boolean");
+                        boolean toWrite = Byte.parseByte(column) != 0;
+                        out.print(Boolean.toString(toWrite));
+                    }
+                    else {
+                        variableTypes.put(StringUtils.toLC(columnLabel), columnClassName);
+                        out.print(column);
+                    }
+                    out.print(delimiter);
+                }
+                String s = resultSet.isLast() ? ";" : ",";
+                out.println(String.format(")%s", s));
+            }
+
+            out.println();
+
+            //class variables
+            variableTypes.forEach((varName, varType) -> {
+                out.println(String.format("private final %s %s;", varType, varName));
+            });
+
+            out.println();
+
+            //enum constructor definition
+            String constructorParameters = variableTypes.entrySet().stream()
+                    .map(varNamevarTypeEntry -> String.format("%s %s", varNamevarTypeEntry.getValue(), varNamevarTypeEntry.getKey()))
+                    .collect(Collectors.joining(","));
+            out.println(String.format("private %s(%s) {", enumName, constructorParameters));
+            variableTypes.forEach((varName, varType) -> {
+                out.println(String.format("this.%s = %s;", varName, varName));
+            });
+            out.println("}");
+
+            out.println();
+
+            //getter methods for class variables
+            variableTypes.forEach((varName, varType) -> {
+                if(varType.equals("boolean")) {
+                    out.println(String.format("public %s is%s() {", varType, StringUtils.toUC(varName)));
+                }
+                else {
+                    out.println(String.format("public %s get%s() {", varType, StringUtils.toUC(varName)));
+                }
+                out.println(String.format("return this.%s;", varName));
+                out.println("}");
+                out.println();
+            });
+
+            out.println("}");
+
+
+
+            closeJavaWriter(out);
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     private String toValidJavaEnumName(String name) {
